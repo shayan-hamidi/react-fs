d702eb3e4d1d4f4a7901af52c819a473d1e24b2f
sh.hamidi <sha.hamidi10@gmail.com>
2024-06-26 00:07:37 +0330

    feat: add tooltip to fs button

--- packages/core/Button/Button.tsx
+++ packages/core/Button/Button.tsx
@@ -1,13 +1,23 @@
-import { Button, ButtonProps } from "@mui/material";
+import { Button, ButtonProps, TooltipProps } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { FsTooltip } from "../Tooltip";
 
 type FsButtonProps = ButtonProps & {
   i18nKey: string;
+  tooltipProps?: Omit<TooltipProps, "title" | "children"> & {
+    i18nKey: string;
+  };
 };
 
-const FsButton = ({ i18nKey, ...rest }: FsButtonProps) => {
+const FsButton = ({ i18nKey, tooltipProps, ...rest }: FsButtonProps) => {
   const { t } = useTranslation();
-  return <Button {...rest}>{t(i18nKey)}</Button>;
+  return tooltipProps ? (
+    <FsTooltip {...tooltipProps}>
+      <Button {...rest}>{t(i18nKey)}</Button>
+    </FsTooltip>
+  ) : (
+    <Button {...rest}>{t(i18nKey)}</Button>
+  );
 };
 
 export default FsButton;
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import reactLogo from "../../assets/react.svg";
 import viteLogo from "../../../public/vite.svg";
+import { FsButton } from "@fs/core";
 
 const Home = () => {
   const [count, setCount] = useState(0);
   return (
     <>
+      <FsButton i18nKey="tooltipppp" tooltipProps={{ i18nKey: "ssss" }} />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
