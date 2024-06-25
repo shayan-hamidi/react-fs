f1cf6fe8d7133ee314e862f2b45554b8dfa43df8
sh.hamidi <sha.hamidi10@gmail.com>
2024-06-25 20:25:31 +0330

    refactor: tooltip

--- packages/core/Tooltip/Tooltip.tsx
+++ packages/core/Tooltip/Tooltip.tsx
@@ -1,63 +1,19 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
+import Tooltip, { TooltipProps } from "@mui/material/Tooltip";
+import { useTranslation } from "react-i18next";
 
-const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip {...props} classes={{ popper: className }} />
-))(({ theme }) => ({
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: theme.palette.common.white,
-    color: 'rgba(0, 0, 0, 0.87)',
-    boxShadow: theme.shadows[1],
-    fontSize: 11,
-  },
-}));
+type FsTooltipProps = Omit<TooltipProps, "title"> & {
+  i18nKey: string;
+};
 
-const BootstrapTooltip = styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip {...props} arrow classes={{ popper: className }} />
-))(({ theme }) => ({
-  [`& .${tooltipClasses.arrow}`]: {
-    color: theme.palette.common.black,
-  },
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: theme.palette.common.black,
-  },
-}));
-
-const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip {...props} classes={{ popper: className }} />
-))(({ theme }) => ({
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: '#f5f5f9',
-    color: 'rgba(0, 0, 0, 0.87)',
-    maxWidth: 220,
-    fontSize: theme.typography.pxToRem(12),
-    border: '1px solid #dadde9',
-  },
-}));
-
-export default function CustomizedTooltips() {
+export default function FsTooltip({
+  children,
+  i18nKey,
+  ...rest
+}: FsTooltipProps) {
+  const { t } = useTranslation();
   return (
-    <div>
-      <LightTooltip title="Add">
-        <Button>Light</Button>
-      </LightTooltip>
-      <BootstrapTooltip title="Add">
-        <Button>Bootstrap</Button>
-      </BootstrapTooltip>
-      <HtmlTooltip
-        title={
-          <React.Fragment>
-            <Typography color="inherit">Tooltip with HTML</Typography>
-            <em>{"And here's"}</em> <b>{'some'}</b> <u>{'amazing content'}</u>.{' '}
-            {"It's very engaging. Right?"}
-          </React.Fragment>
-        }
-      >
-        <Button>HTML</Button>
-      </HtmlTooltip>
-    </div>
+    <Tooltip title={t(i18nKey)} {...rest}>
+      {children}
+    </Tooltip>
   );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { FsButton } from "../packages/core/Button";
 import { useTranslation } from "react-i18next";
 import { addTranslationSchema } from "./i18nConfig";
 import faTranslations from "src/i18n/fa.json";
+import { FsTooltip } from "@fs/core";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,7 +14,10 @@ function App() {
   ready && addTranslationSchema("fa", faTranslations);
   return (
     <>
-      <FsButton i18nKey={"OK"} />
+      <FsButton i18nKey="OK" />
+      <FsTooltip i18nKey="NO">
+        <FsButton i18nKey="OK" />
+      </FsTooltip>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
