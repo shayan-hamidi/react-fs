30500ad011ebd84d5ba6e98d1e36d83f0d7fa407
sh.hamidi <sha.hamidi10@gmail.com>
2024-07-11 18:22:42 +0330

    feat(package): handle multiple in autocomplete

--- packages/form/AutoComplete/AutoComplete.tsx
+++ packages/form/AutoComplete/AutoComplete.tsx
@@ -5,21 +5,23 @@ import { useTranslation } from "react-i18next";
 
 type TOptions = { value: string; label: string };
 
-type FsAutoCompleteProps = Omit<
-  AutocompleteProps<TOptions, false, false, false>,
+type FsAutoCompleteProps<TMultiple extends boolean | undefined> = Omit<
+  AutocompleteProps<TOptions, TMultiple, false, false>,
   "renderInput" | "options"
 > & {
   i18nKey: string;
   name: string;
   options: TOptions[];
+  multiple?: TMultiple;
 };
 
-const FsAutoComplete = ({
+function FsAutoComplete<TMultiple extends boolean | undefined>({
   name,
   i18nKey,
   options,
+  multiple,
   ...rest
-}: FsAutoCompleteProps) => {
+}: FsAutoCompleteProps<TMultiple>) {
   const { t } = useTranslation();
   const { control } = useFormContext();
 
@@ -27,14 +29,28 @@ const FsAutoComplete = ({
     <Controller
       name={name}
       control={control}
-      defaultValue={null}
+      defaultValue={multiple ? [] : undefined}
       render={({ field: { onChange, value, ref } }) => (
         <Autocomplete
           {...rest}
+          multiple={multiple}
           options={options}
-          value={options.find((option) => option.value === value) || null}
-          onChange={(_event, newValue: TOptions | null) => {
-            onChange(newValue ? newValue.value : null);
+          value={
+            multiple
+              ? value &&
+                options.filter((option) => value.includes(option.value))
+              : value
+              ? options.find((option) => option.value === value)
+              : null
+          }
+          onChange={(_event, newValue) => {
+            onChange(
+              multiple
+                ? (newValue as TOptions[]).map((item) => item.value)
+                : newValue
+                ? (newValue as TOptions).value
+                : undefined
+            );
           }}
           getOptionLabel={(option) => option.label}
           ref={ref}
@@ -45,6 +61,6 @@ const FsAutoComplete = ({
       )}
     />
   );
-};
+}
 
 export default FsAutoComplete;
