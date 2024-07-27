0e1a929c8ede7ec2f7ed4ee34de929c091d5de66
sh.hamidi <sha.hamidi10@gmail.com>
2024-07-27 22:28:19 +0330

    feat(package): add date picker to form

--- packages/form/DatePicker/DatePicker.tsx
+++ packages/form/DatePicker/DatePicker.tsx
@@ -1,36 +1,37 @@
-import { AdapterDateFnsJalali } from "@mui/x-date-pickers/AdapterDateFnsJalali";
+import { AdapterMomentJalaali } from "@mui/x-date-pickers/AdapterMomentJalaali";
+import { DatePicker, DatePickerProps } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-// import useTheme from '@mui/system/useTheme';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
+import moment from "moment-jalaali";
 import { Controller, ControllerProps, useFormContext } from "react-hook-form";
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
-type FsDatePickerProps = {
+type FsDatePickerProps = DatePickerProps<moment> & {
   i18nKey: string;
   rules?: ControllerProps["rules"];
   name: string;
-  defaultValue?: string | number | boolean;
+  defaultValue?: Date;
 };
 
-const FsDatePicker = ({ rules }: FsDatePickerProps) => {
+const FsDatePicker = ({
+  rules,
+  name,
+  i18nKey,
+  defaultValue,
+  ...rest
+}: FsDatePickerProps) => {
   const { control } = useFormContext();
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
+  moment.loadPersian({ dialect: "persian-modern" });
   return (
     <Controller
-      name={"d"}
+      name={name}
       control={control}
       rules={rules}
-      defaultValue={null}
-      render={() => (
-        <div dir="rtl">
-          <LocalizationProvider dateAdapter={AdapterDateFnsJalali}>
-            <DateTimePicker
-              label="AdapterDateFnsJalali"
-              defaultValue={new Date(2022, 1, 1, 12)}
-            />
-          </LocalizationProvider>
-        </div>
+      defaultValue={defaultValue}
+      render={({ field }) => (
+        <LocalizationProvider dateAdapter={AdapterMomentJalaali}>
+          <DatePicker label={t(i18nKey)} {...field} {...rest} />
+        </LocalizationProvider>
       )}
     />
   );
