55863ce57cf2653d208a34c75b970a5230e9cfab
s.hamidi <sha.hamidi10@gmail.com>
2024-12-16 16:53:24 +0330

    feat: update login background compatible with theme

--- src/pages/Login/components/index.tsx
+++ src/pages/Login/components/index.tsx
@@ -5,9 +5,21 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-import backgroundLogin from 'src/assets/images/background-login.jpg';
+import defaultLBg from 'src/assets/images/default-L-login-bg.jpg';
+import defaultDBg from 'src/assets/images/default-D-login-bg.jpg';
+import purpleLBg from 'src/assets/images/purple-L-login-bg.jpg';
+import purpleDBg from 'src/assets/images/purple-D-login-bg.jpg';
+import yellowLBg from 'src/assets/images/yellow-L-login-bg.jpg';
+import yellowDBg from 'src/assets/images/yellow-D-login-bg.jpg';
+import brownLBg from 'src/assets/images/brown-L-login-bg.jpg';
+import brownDBg from 'src/assets/images/brown-D-login-bg.jpg';
+import greenLBg from 'src/assets/images/green-L-login-bg.jpg';
+import greenDBg from 'src/assets/images/green-D-login-bg.jpg';
+import blueLBg from 'src/assets/images/blue-L-login-bg.jpg';
+import blueDBg from 'src/assets/images/blue-D-login-bg.jpg';
 import logo from 'src/assets/images/logo.png';
 import { Footer } from 'src/common/components/Layout';
+import { useFsTheme } from '@fs/utils';
 
 const Login = () => {
   const [_captchaGuid, setCaptchaGuid] = useState('');
@@ -15,16 +27,53 @@ const Login = () => {
   const { triggerAlert } = useAlert();
   const methods = useForm();
   const navigate = useNavigate();
+  const { mode, themeTemplate } = useFsTheme();
   const { t } = useTranslation();
   const onSubmit = () => {
     navigate('/');
     triggerAlert('ورود با موفقیت انجام شد', 2000);
   };
-
+  const getLoginBg = () => {
+    if (mode === 'light') {
+      switch (themeTemplate) {
+        case 'default':
+          return defaultLBg;
+        case 'blue':
+          return blueLBg;
+        case 'brown':
+          return brownLBg;
+        case 'green':
+          return greenLBg;
+        case 'purple':
+          return purpleLBg;
+        case 'yellow':
+          return yellowLBg;
+        default:
+          return defaultLBg;
+      }
+    } else {
+      switch (themeTemplate) {
+        case 'default':
+          return defaultDBg;
+        case 'blue':
+          return blueDBg;
+        case 'brown':
+          return brownDBg;
+        case 'green':
+          return greenDBg;
+        case 'purple':
+          return purpleDBg;
+        case 'yellow':
+          return yellowDBg;
+        default:
+          return defaultDBg;
+      }
+    }
+  };
   return (
     <Box
       sx={{
-        backgroundImage: `url(${backgroundLogin})`,
+        backgroundImage: `url(${getLoginBg()})`,
         backgroundSize: '100% 50vh',
         backgroundPosition: 'top',
         backgroundRepeat: 'no-repeat',
@@ -51,7 +100,7 @@ const Login = () => {
             borderRadius: '0.75rem',
           }}
         >
-          <img src={logo} alt="site logo" />
+          <img src={logo} alt="site logo" width={150} />
           <FsTypography
             i18nKey={'LOGIN_IN_SYSTEM'}
             color={'primary'}
